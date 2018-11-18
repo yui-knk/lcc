30d66758dfc58b91f44a17b4bdbb6b175ee85097
yui-knk <spiketeika@gmail.com>
2018-11-18 21:19:52 +0900

    `add` and `sub` (Step 2)

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 int
 main(int argc, char** argv)
 {
+    char *p = argv[1];
+
     if (argc != 2) {
         fprintf(stderr, "One argument should be passed (%d is passed).\n", argc - 1);
         return 1;
@@ -12,8 +14,25 @@ main(int argc, char** argv)
     printf(".intel_syntax noprefix\n");
     printf(".global main\n\n");
     printf("main:\n");
-    printf("        mov rax, %d\n", atoi(argv[1]));
-    printf("        ret\n");
+    printf("        mov rax, %ld\n", strtol(p, &p, 10));
 
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("        add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("        sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected char '%c'\n", *p);
+        return 1;
+    }
+
+    printf("        ret\n");
     return 0;
 }
